6b0dd6185e9841c7372a7e0c20b539f96c0c7478
liangmuren <409771576@qqcom>
2016-11-23 15:16:16 +0800

    Finshed without testintg

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+app.use('/:time',function(req,res){
+    var time = req.params.time
+    var unixtime = null
+    var naturaltime = null
+    if(!isNaN(time)){
+        unixtime = time
+        var date = new Date(Number(time)*1000)
+        naturaltime=monthName[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear()
+    }else if(!isNaN(Date.parse(time))){
+        date = new Date(time)
+        unixtime = Date.parse(date)/1000
+        naturaltime = time
+    }
+    var obj = {unix:unixtime,natural:naturaltime}
+    // var obj = showdate(date)
+    res.send(JSON.stringify(obj))
+})
+app.listen(3000);
+var monthName = ["January","February","March","April","May","June","July","August","September","October","November","December"]
